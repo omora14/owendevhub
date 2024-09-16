da9db20902b805a9f3b19c6f8e127971f68b5b75
Owen Morales <morales.owen@icloud.com>
2024-09-16 10:49:59 -0600

    hotfix(2)

--- portfolio/forms/contact.php
+++ portfolio/forms/contact.php
@@ -1,34 +1,33 @@
 <?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-  $receiving_email_address = 'morales.owen@icloud.com';
+require '../vendor/autoload.php'; 
 
-  if( file_exists($php_email_form = '../assets/vendor/php-email-form/php-email-form.php' )) {
-    include( $php_email_form );
-  } else {
-    die( 'Unable to load the "PHP Email Form" Library!');
-  }
+$mail = new PHPMailer(true);
 
-  $contact = new PHP_Email_Form;
-  $contact->ajax = true;
-  
-  $contact->to = $receiving_email_address;
-  $contact->from_name = $_POST['name'];
-  $contact->from_email = $_POST['email'];
-  $contact->subject = $_POST['subject'];
+try {
+  //Server settings
+  $mail->isSMTP();
+  $mail->Host = 'smtp.gmail.com'; 
+  $mail->SMTPAuth = true;
+  $mail->Username = 'your-email@gmail.com'; 
+  $mail->Password = 'your-email-password'; 
+  $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;
+  $mail->Port = 587;
 
-  // I need to enter my correct SMTP credentials
+  //Recipients
+  $mail->setFrom($_POST['email'], $_POST['name']);
+  $mail->addAddress('morales.owen@icloud.com'); 
 
-  $contact->smtp = array(
-    'host' => 'HOST_TOKEN',
-    'username' => 'USERNAME',
-    'password' => 'SECRET',
-    'port' => 'SECRET_PORT'
-  );
+  //Content
+  $mail->isHTML(true);
+  $mail->Subject = $_POST['subject'];
+  $mail->Body    = nl2br($_POST['message']); 
 
-
-  $contact->add_message( $_POST['name'], 'From');
-  $contact->add_message( $_POST['email'], 'Email');
-  $contact->add_message( $_POST['message'], 'Message', 10);
-
-  echo $contact->send();
+  $mail->send();
+  echo 'Message has been sent';
+} catch (Exception $e) {
+  echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
 ?>
